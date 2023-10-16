efa00bf017bfd5eade0ced06cf90b4418d0b4ca6
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-10-16 09:46:52 +0530

    Conversion of KM/Hr to Meter/Sec

--- PracticeQuestion34.java
+++ PracticeQuestion34.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class PracticeQuestion34 {
+import java.util.Scanner;
+
+public class PracticeQuestion34 {
+    /*Problem Description
+    Given the speed of a car in kilometers per hour,
+    return its speed in meters per second.*/
+
+    public static int kmPerHourToMeterPerSecond(int km) {
+        return (km * 1000) / 3600;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter distance in Km: ");
+        int km = scanner.nextInt();
+        int result = kmPerHourToMeterPerSecond(km);
+        System.out.println("Conversion of Km/Hr to Meter/Sec is: " + result);
+    }
 }
